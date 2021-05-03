2b1827dc3f21b9cb7c1af1aafc3f2f1edbec0c8d
street-m3 <65938603+street-m3@users.noreply.github.com>
2021-05-03 20:54:36 +0900

    clean: minify

--- assets/dist/js/noopener.js
+++ assets/dist/js/noopener.js
@@ -1,16 +1,1 @@
-'use strict';
-// == target_blankを取得 rel属性の付与 ==
-function linkersTarget() {
-    const linksTarget = document.querySelectorAll('a');
-    linksTarget.forEach(elem => {
-        if (elem.hasAttribute('target') === false) {
-            return;
-        }
-        if (elem.getAttribute('target') !== '_blank') {
-            return;
-        }
-        elem.setAttribute('rel', 'noopener noreferrer');
-    });
-}
-
-linkersTarget();
+"use strict";function linkersTarget(){const t=document.querySelectorAll("a");t.forEach(t=>{!1!==t.hasAttribute("target")&&"_blank"===t.getAttribute("target")&&t.setAttribute("rel","noopener noreferrer")})}linkersTarget();
