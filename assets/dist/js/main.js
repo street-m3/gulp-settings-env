6e51c713774a1cc886e88c8919481d1e5e85c35d
street-m3 <65938603+street-m3@users.noreply.github.com>
2021-05-04 12:36:47 +0900

    upgrade: brタグが存在する場合、aria-hiddenを付与するscriptを追加

--- assets/dist/js/main.js
+++ assets/dist/js/main.js
@@ -1,1 +1,14 @@
-"use strict";
+'use strict';
+
+/**
+ * target_blankを取得 rel属性の付与
+ */
+const textBreaks = () => {
+    const breakElements = document.querySelectorAll('br');
+    breakElements.forEach(element => {
+        if (element.hasAttribute('aria-hidden') !== false) return;
+        element.setAttribute('aria-hidden', 'true');
+    });
+}
+
+textBreaks();
