1a97a58a7b34e367b821a5d4af53a38314569462
street-m3 <65938603+street-m3@users.noreply.github.com>
2021-05-04 12:37:54 +0900

    clean: コードを短略化

--- assets/dist/js/noopener.js
+++ assets/dist/js/noopener.js
@@ -1,1 +1,13 @@
-"use strict";function linkersTarget(){const t=document.querySelectorAll("a");t.forEach(t=>{!1!==t.hasAttribute("target")&&"_blank"===t.getAttribute("target")&&t.setAttribute("rel","noopener noreferrer")})}linkersTarget();
+'use strict';
+
+/**
+ * target_blankを取得 rel属性の付与
+ */
+const linkersTarget = () => {
+    const linksTarget = document.querySelectorAll('a');
+    linksTarget.forEach(element => {
+        if (element.hasAttribute('target') === false || element.getAttribute('target') !== '_blank') return;
+        element.setAttribute('rel', 'noopener noreferrer');
+    });
+}
+linkersTarget();
--- assets/src/js/noopener.js
+++ assets/src/js/noopener.js
@@ -1,16 +1,13 @@
 'use strict';
-// == target_blankを取得 rel属性の付与 ==
-function linkersTarget() {
+
+/**
+ * target_blankを取得 rel属性の付与
+ */
+const linkersTarget = () => {
     const linksTarget = document.querySelectorAll('a');
-    linksTarget.forEach(elem => {
-        if (elem.hasAttribute('target') === false) {
-            return;
-        }
-        if (elem.getAttribute('target') !== '_blank') {
-            return;
-        }
-        elem.setAttribute('rel', 'noopener noreferrer');
+    linksTarget.forEach(element => {
+        if (element.hasAttribute('target') === false || element.getAttribute('target') !== '_blank') return;
+        element.setAttribute('rel', 'noopener noreferrer');
     });
 }
-
 linkersTarget();
